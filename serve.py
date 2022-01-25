4ab1c20ee4be84ac7aba7db26965903e0359cead
Hartley Brody <hartley.brody@gmail.com>
2022-01-25 17:31:30 -0500

    setup simple http server in background

--- serve.py
+++ serve.py
@@ -1,10 +1,26 @@
 import time
 import hashlib
+import threading
+import http.server
 
 from build import build
-from utils import get_template_paths
+from utils import get_template_paths, OUTPUT_PREFIX
+
+# override SimpleHTTPRequestHandler to serve files from OUTPUT_PREFIX
+# via https://stackoverflow.com/a/52531444/625840
+class Handler(http.server.SimpleHTTPRequestHandler):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, directory=OUTPUT_PREFIX, **kwargs)
+
+def start_server():
+    httpd = http.server.HTTPServer(('', 8383), Handler)
+    httpd.serve_forever()
 
 def serve():
+
+    # start server in the background so we can watch files for changes
+    server_thread = threading.Thread(target=start_server, name="local_server")
+    server_thread.start()
 
     build()  # initial build
     file_hashes = {}
