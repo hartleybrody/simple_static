030d5d77b47aca095059f13fd6066f0b49304749
Hartley Brody <hartley.brody@gmail.com>
2022-01-25 15:06:01 -0500

    better logging

--- build.py
+++ build.py
@@ -4,29 +4,35 @@ from glob import glob
 
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 
+INPUT_PREFIX = "site"
+OUTPUT_PREFIX = "_build"
+
 def build():
 
     env = Environment(
-        loader=FileSystemLoader("site"),
+        loader=FileSystemLoader(INPUT_PREFIX),
         autoescape=select_autoescape()
     )
 
     try:
-        shutil.rmtree("_build")  # nuke everything
+        shutil.rmtree(OUTPUT_PREFIX)  # nuke everything
     except FileNotFoundError:
         pass
-    os.makedirs("_build")
+    os.makedirs(OUTPUT_PREFIX)
 
-    for path in glob("site/**/*.html", recursive=True):
+    print(f"building site from {os.sep}{INPUT_PREFIX}{os.sep} to {os.sep}{OUTPUT_PREFIX}{os.sep}")
+
+    for path in glob(f"{INPUT_PREFIX}/**/*.html", recursive=True):
         path = os.path.normpath(path)
 
-        f = path.split("site/")[1]
+        f = path.split(f"{INPUT_PREFIX}{os.sep}")[1]
         template = env.get_template(f)
-        print(f"======= {f} =======")
 
         output_path = generate_render_output_path(f)
         if not output_path:
             continue
+
+        print(f" {f:<20}\t==> {output_path}")
 
         os.makedirs(os.path.dirname(output_path), exist_ok=True)  # https://stackoverflow.com/a/12517490/625840
         with open(output_path, "w+") as f:
@@ -38,7 +44,7 @@ def generate_render_output_path(f):
     pieces[-1] = pieces[-1].replace(".html", "")  # strip file extension
     if pieces[-1].startswith("base"):
         return False
-    return os.path.join(os.getcwd(), "_build/", *pieces, "index.html")
+    return os.path.join(os.getcwd(), OUTPUT_PREFIX, *pieces, "index.html")
 
 
 if __name__ == '__main__':
