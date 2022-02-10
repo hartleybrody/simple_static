1c40e9d85d67b6f8d4624d7adec436b699218e9a
Hartley Brody <hartley.brody@gmail.com>
2022-02-10 15:50:40 -0500

    can't read dependencies at build time :(, must hard-code

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 import setuptools
 
-# read list of dependencies from requirements.txt
-with open('requirements.txt', 'r') as f:
-    deps = f.read().split("\n")
-
 setuptools.setup(
     name='simple_static',
     version='0.0.1',
     packages=['simple_static'],
-    install_requires=deps,
+    install_requires=[
+        "Jinja2",
+        "MarkupSafe",
+        "PyYAML",
+    ],
     entry_points={
         'console_scripts': [
             'build = simple_static.build:build',
