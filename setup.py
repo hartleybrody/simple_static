da1008195cefd42cbad6fd777d7aad14e596cbd1
Hartley Brody <hartley.brody@gmail.com>
2022-02-11 14:55:18 -0500

    bump version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name='simple_static',
-    version='0.0.1',
+    version='0.0.2',
     packages=['simple_static'],
     install_requires=[
         "Jinja2",
