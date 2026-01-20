cb7ff9005e014df0a95e8b89c388b4081e3d34a9
Hartley Brody <hartley.brody@gmail.com>
2026-01-20 12:25:41 -0500

    bump package version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name='simple_static',
-    version='0.0.3',
+    version='0.0.4',
     packages=['simple_static'],
     install_requires=[
         "Jinja2",
